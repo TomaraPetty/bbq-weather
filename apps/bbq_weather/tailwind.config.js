7e652b5c5cedb3cd4ee82af39eb69abb155bedd1
Tomara Petty <tomararuth@gmail.com>
2025-05-11 11:26:47 -0700

    add shadcn with necessary components

--- apps/bbq_weather/tailwind.config.js
+++ apps/bbq_weather/tailwind.config.js
@@ -12,7 +12,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
+    './app/**/*.{ts,tsx,js,jsx,html}',
+    './components/**/*.{ts,tsx,js,jsx,html}',
+    './lib/**/*.{ts,tsx,js,jsx,html}',
     '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
     //     ...createGlobPatternsForDependencies(__dirname)
   ],
