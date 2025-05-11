688fd42cf0fd0dd7f2f75c00f83e12d9dd5ce0d4
Tomara Petty <tomararuth@gmail.com>
2025-05-10 20:16:13 -0700

    add home page with some text

---
+++ apps/bbq_weather/app/home/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Is it BBQ weather?</h1>
+      <p>
+        The weather is currently 21&deg;C and cloudy with a chance of meat
+        burning.
+      </p>
+    </div>
+  );
+}
