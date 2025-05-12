ab8e21ad5376f20e40992890ec3a83d20ed0e501
Tomara Petty <tomararuth@gmail.com>
2025-05-11 19:21:05 -0700

    add jest setup

--- apps/bbq_weather/jest.config.ts
+++ apps/bbq_weather/jest.config.ts
@@ -1,12 +1,11 @@
 const config = {
-  displayName: 'bbq_weather',
+  displayName: 'bbq-weather',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: 'test-output/jest/coverage',
+  coverageDirectory: '<rootDir>/test-output/jest/coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
---
+++ apps/bbq_weather/jest.setup.ts
@@ -0,0 +1,1 @@
+// intentionally empty setup file for Jest 
