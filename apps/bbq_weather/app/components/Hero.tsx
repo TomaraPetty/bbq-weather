b8cbc634e6e8fee00d69f61d189940ce32ec02b8
Tomara Petty <tomararuth@gmail.com>
2025-05-12 13:08:43 -0700

    move dialog component out of page into its own component

---
+++ apps/bbq_weather/app/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Hero() {
+  return (
+    <>
+      <h1 className="text-8xl font-bold text-white mb-6">
+        Suns out, grills out!
+      </h1>
+      <p className="text-2xl text-white/90 mb-4">
+        Check the weather for your area to see if you should fire up the
+        grill this week.
+      </p>
+    </>
+  );
+}
