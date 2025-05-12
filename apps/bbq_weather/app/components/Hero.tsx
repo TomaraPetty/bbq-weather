9205696dd725fe7ad59af07e8d908a41796c5645
Tomara Petty <tomararuth@gmail.com>
2025-05-12 14:37:47 -0700

    change back to one week

--- apps/bbq_weather/app/components/Hero.tsx
+++ apps/bbq_weather/app/components/Hero.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <>
+    <div className="flex flex-col items-center text-center justify-center mx-auto w-3/5">
       <h1 className="text-8xl font-bold text-white mb-6">
         Suns out, grills out!
       </h1>
       <p className="text-2xl text-white/90 mb-4">
         Check the weather in your area to see if you should fire up the
-        grill today.
+        grill this week.
       </p>
-    </>
+    </div>
   );
 }
