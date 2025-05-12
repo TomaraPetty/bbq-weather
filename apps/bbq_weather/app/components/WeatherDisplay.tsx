1fb208d0ed81befd7f7fffbc4bcfcc91c912700f
Tomara Petty <tomararuth@gmail.com>
2025-05-12 14:38:51 -0700

    bbq evaluator to determine days to grill and add function to convert c to f

--- apps/bbq_weather/app/components/WeatherDisplay.tsx
+++ apps/bbq_weather/app/components/WeatherDisplay.tsx
@@ -15,6 +15,11 @@ export default function WeatherDisplay({
   city,
 }: WeatherDisplayProps) {
   const currentWeather = weather?.list?.[0];
+  const bbqEvaluator = currentWeather ? weather?.list.filter((weather) => {
+    const tempF = cToF(weather.main.temp);
+    return tempF > 60 && tempF < 85;
+  }) : [];
+  const bbqDays = currentWeather ? bbqEvaluator?.length : 0;
   try {
     if (!city) {
       return (
@@ -34,11 +39,13 @@ export default function WeatherDisplay({
         <div className="text-xl text-white/90">
           <p>
             The weather in {city} is currently{' '}
-            {Math.round(currentWeather.main.temp)}
-            &deg;C and {currentWeather.weather[0].description}
+            {Math.round(cToF(currentWeather.main.temp))}&deg;F and {currentWeather.weather[0].description}
           </p>
           <p className="mt-2">
-            Feels like {Math.round(currentWeather.main.feels_like)}&deg;C
+            Feels like {Math.round(cToF(currentWeather.main.feels_like))}&deg;F
+          </p>
+          <p className="mt-2">
+            You can grill {bbqDays} days this week.
           </p>
         </div>
       );
@@ -48,10 +55,14 @@ export default function WeatherDisplay({
   }
   return (
     <>
-      <p className="text-xl text-white/90">
+      <p className="text-xl text-white/90 mx-auto w-3/5">
         That might not have been a proper city name. Please try again but this
         time don&apos;t mess up.
       </p>
     </>
   );
 }
+function cToF(temp: number) {
+  return temp * 9 / 5 + 32;
+}
+
