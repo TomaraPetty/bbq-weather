1acd78c5975ce4609374d85745543803b309e52a
Tomara Petty <tomararuth@gmail.com>
2025-05-11 19:01:04 -0700

    fix lint errors

--- apps/bbq_weather/jest.config.ts
+++ apps/bbq_weather/jest.config.ts
@@ -1,10 +1,14 @@
-export default {
+const config = {
   displayName: 'bbq_weather',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: 'test-output/jest/coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
+
+export default config;
