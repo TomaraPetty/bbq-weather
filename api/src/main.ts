69bc572f26a2b98bacfb7eb953555796df48521e
Tomara Petty <tomararuth@gmail.com>
2025-05-13 11:51:53 -0700

    add swagger setup to main ts in api

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,13 +5,24 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  const port = process.env.PORT || 5232;
+  const port = process.env.PORT ?? 5232;
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('BBQ Weather API')
+    .setDescription('The BBQ Weather API description')
+    .setVersion('1.0')
+    .addTag('bbq-weather')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${'api'}`
