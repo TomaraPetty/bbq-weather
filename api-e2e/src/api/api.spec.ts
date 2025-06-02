ca2a08f47e2bb1e7193a8b16f91d4d3659754785
Tomara Petty <tomararuth@gmail.com>
2025-06-01 20:04:18 -0700

    update e2e to be a real test to check the weather api

--- api-e2e/src/api/api.spec.ts
+++ api-e2e/src/api/api.spec.ts
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+describe('Test Weather Forecast API', () => {
+  it('should return weather data for a city', async () => {
+    const res = await axios.get(`http://localhost:5232/api/weather/forecast?city=denver`);
+    const city = await res.data['city']['name']
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
-  });
-
-  it('dummy test', () => {
-    expect(1).toBe(1);
+    expect(city).toEqual('Denver');
   });
 });
